e9a621b8f7e3271aa8d1e3107d358333a64a7ccc
DavidJohnathan <dkenna99@gmail.com>
2020-02-10 09:40:49 +0000

    Initial

---
+++ aes.py
@@ -0,0 +1,18 @@
+def key_expansion(key):
+    """
+    128 bit key -> 11 128 bit keys
+    """
+    return None
+
+def key_add(state, round_key):
+
+    return None
+
+def sub_bytes(state):
+    return None
+
+def shift_rows(state):
+    return None
+
+def aes_enc(key, message):
+    return None
